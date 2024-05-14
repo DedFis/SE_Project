1ec5cf1ababe997ad27afef91b601727bd2ba9b5
Madeline Emily <93426401+madelinemily@users.noreply.github.com>
2024-05-14 18:43:28 +0700

    Progress 14 Mei pt.2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home';
+import Shops from './pages/Shops';
+import Card from './pages/Card';
+import Details from './pages/Details';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />}></Route>
+      <Route path='/shops' element={<Shops />}></Route>
+      <Route path='/card' element={<Card />}></Route>
+      <Route path='/product/details/:slug' element={<Details />}></Route>
     </Routes>
     </BrowserRouter>
   );
