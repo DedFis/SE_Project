f467a6e093240bd427e6d75344353e9e1765ad78
Madeline Emily <93426401+madelinemily@users.noreply.github.com>
2024-05-15 15:06:50 +0700

    Login Register customer done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,15 @@ import Home from './pages/Home';
 import Shops from './pages/Shops';
 import Card from './pages/Card';
 import Details from './pages/Details';
+import Register from './pages/Register';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
+      <Route path='/register' element={<Register />}></Route>
+      <Route path='/login' element={<Login />}></Route>
       <Route path='/' element={<Home />}></Route>
       <Route path='/shops' element={<Shops />}></Route>
       <Route path='/card' element={<Card />}></Route>
