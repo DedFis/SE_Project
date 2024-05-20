fc3a47b48b9d38a36c483ff519f3d9b19b159517
DeadFish <medygunawan32@gmail.com>
2024-05-20 12:02:37 +0700

    Headers small details fix

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -82,7 +82,7 @@ const Headers = () => {
                                         <Link to='/' className={`p-2 block ${pathname === '/' ? 'text-[#46c6c7]' : 'text-slate-600'}`}>Home</Link>
                                     </li>
                                     <li>
-                                        <Link to='/shops' className={`p-2 block ${pathname === '/shop' ? 'text-[#7fad39]' : 'text-slate-600'}`}>Shop</Link>
+                                        <Link to='/shops' className={`p-2 block ${pathname === '/shops' ? 'text-[#7fad39]' : 'text-slate-600'}`}>Shop</Link>
                                     </li>
                                     {/* <li>
                                         <Link className={`p-2 block ${pathname === '/blog' ? 'text-[#7fad39]' : 'text-slate-600'}`}>Blog</Link>
