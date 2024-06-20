a0b295b75c525dd2a6b91f9828e6cef6231a1cc8
Madeline Emily <93426401+madelinemily@users.noreply.github.com>
2024-06-20 09:29:56 +0700

    logout functional and header username active details

--- dashboard/src/layout/Header.jsx
+++ dashboard/src/layout/Header.jsx
@@ -1,7 +1,8 @@
 import {FaList} from 'react-icons/fa'
+import {useSelector} from 'react-redux'
 
 const Header = ({showSidebar, setShowSidebar}) => {
-
+    const {userInfo} = useSelector(state=>state.auth)
     return ( 
         <div className="fixed top-0 left-0 w-full py-5 px-2 lg:px-7 z-40">
             <div className="ml-0 lg:ml-[250px] rounded-md h-[65px] flex justify-between items-center bg-blue-900 text-[#d0d2d6] px-5 transition-all">
@@ -15,8 +16,8 @@ const Header = ({showSidebar, setShowSidebar}) => {
                     <div className='flex justify-center items-center'>
                         <div className='flex justify-center items-center gap-3'>
                             <div className='flex justify-center items-center flex-col text-end'>
-                                <h2 className='text-sm font-bold'>Madeline Emily</h2>
-                                <span className='text-[14px] w-full font-normal'>Admin</span>
+                                <h2 className='text-sm font-bold'>{userInfo.name}</h2>
+                                <span className='text-[14px] w-full font-normal'>{userInfo.role}</span>
                             </div>
                             <img className='w-[45px] h-[45px] rounded-full overflow-hidden' src="http://localhost:3000/images/duck.jpg" alt="" />
                         </div>
--- dashboard/src/layout/Sidebar.jsx
+++ dashboard/src/layout/Sidebar.jsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { useSelector } from 'react-redux';
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { getNavs } from "../navigation/index";
+import { logout } from "../store/Reducers/authReducer";
 import { BiLogInCircle } from "react-icons/bi";
+import { useDispatch } from "react-redux";
 
 const Sidebar = ({ showSidebar, setShowSidebar }) => {
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
   const { role } = useSelector(state => state.auth)
   const { pathname } = useLocation();
   const [allNav, setAllNav] = useState([]);
@@ -54,7 +58,7 @@ const Sidebar = ({ showSidebar, setShowSidebar }) => {
               </li>
             ))}
             <li>
-              <button className="text-[#d0d2d6] font-normal duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1">
+              <button onClick={()=>dispatch(logout({navigate, role}))} className="text-[#d0d2d6] font-normal duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1">
                 <span>
                   <BiLogInCircle />
                 </span>
